0e83fdc097b7792f522ebb18cd8dbbeab7dd2ea9
Arpita Sawant <97539573+arpitasawant@users.noreply.github.com>
2023-05-28 10:56:16 +0530

    Update logout2.php

--- logout2.php
+++ logout2.php
@@ -4,6 +4,9 @@ session_start();
 session_unset();
 session_destroy();
 echo "Succesfully logout!";
+//Method 1
 //header("Location : index.php");
+
+//Method 2
 header('Refresh: 2; URL = index.php');
 
